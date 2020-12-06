19e57971464c8b072b6dd403c0819dfcd1025140
Meron Lindkvist <meron.hagos2@gmail.com>
2020-12-06 22:37:52 +0100

    Uppdaterat lite

--- src/ManageEmployees.java
+++ src/ManageEmployees.java
@@ -3,6 +3,8 @@ import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 public class ManageEmployees  {
     private JFrame frame = new JFrame("Employee Register");
@@ -37,8 +39,37 @@ public class ManageEmployees  {
         button1= new JButton("Add");
         button1.setSize(100,30);
         button1.setLocation(50,370);
-      //  button1.addActionListener(new ());
+        button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFrame addFrame= new JFrame();
+                JLabel addName= new JLabel("Namn");
+                JButton buttonSave,buttonCancel;
+                addName.setSize(80,30);
+                addName.setLocation(10,40);
+
+                JTextField namnField= new JTextField(50);
+                namnField.setSize(200,30);
+                namnField.setLocation(60,50);
+                addFrame.setLayout(null);
+                addFrame.setSize(300,400);
+                //addFrame.setDefaultCloseOperation(addFrame.EXIT_ON_CLOSE);
+                addFrame.add(addName);
+                addFrame.add(namnField);
+                addFrame.setVisible(true);
+                buttonSave= new JButton("Save");
+                buttonCancel=new JButton("Cancel");
+                buttonSave.setSize(80,30);
+                buttonSave.setLocation(50,300);
+                buttonCancel.setSize(80,30);
+                buttonCancel.setLocation(170,300);
+                addFrame.add(buttonSave);
+                addFrame.add(buttonCancel);
+
+            }
+        });
         frame.add(button1);
+
 
         button2= new JButton("Delete");
         button2.setSize(100,30);
@@ -52,11 +83,11 @@ public class ManageEmployees  {
        // button3.addActionListener(new ());
         frame.add(button3);
 
-        button4=new JButton("Search");
+        /*button4=new JButton("Search");
         button4.setSize(100,30);
         button4.setLocation(650,370);
        // button4.addActionListener(new ());
-        frame.add(button4);
+        frame.add(button4);*/
 
 
         label1 = new JLabel("Search");
@@ -67,10 +98,21 @@ public class ManageEmployees  {
         textField= new JTextField(10);
         textField.setSize(150,20);
         textField.setLocation(600,15);
-        textField.addActionListener(new ActionListener() {
+        textField.addKeyListener(new KeyListener() {
             @Override
-            public void actionPerformed(ActionEvent e) {
+            public void keyTyped(KeyEvent e) {
+                String textToSearch = textField.getText();
+                Object[][] filteredEmployeeInfo = getFilteredEmployeeInfo(textToSearch, employeeInfo);
+                tabelmodel=new DefaultTableModel(filteredEmployeeInfo,columnnames);
+                table.setModel(tabelmodel);        
+            }
 
+            @Override
+            public void keyPressed(KeyEvent e) {
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
             }
         });
         frame.add(textField);
@@ -81,6 +123,15 @@ public class ManageEmployees  {
 
 
 }
+
+    private Object[][] getFilteredEmployeeInfo(String textToSearch, Object[][] employeeInfo) {
+        if(textToSearch.isEmpty()) {
+            return employeeInfo;
+        }
+        Object[][] filteredEmployeeInfo = {{"Christian","","","","","","",""}};
+        return filteredEmployeeInfo;
+    }
+
     public static void main(String[] args) {
        new ManageEmployees();
 
