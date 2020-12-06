d55bebcbe58848b4d88311c2d4d935b92a595d04
abelshif <72088379+abelshif@users.noreply.github.com>
2020-12-06 14:57:31 +0100

    Created login and home page GUI

---
+++ src/LoginSystem/HomePage.java
@@ -0,0 +1,30 @@
+package LoginSystem;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class HomePage extends JFrame {
+
+    JFrame mainFrame;
+    JPanel homePagePanel;
+
+    public HomePage (){
+
+        mainFrame = new JFrame();
+        homePagePanel = new JPanel();
+
+        homePagePanel.setLayout(null);
+        homePagePanel.setVisible(true);
+        homePagePanel.setBackground(Color.CYAN);
+
+        add(homePagePanel);
+        setTitle("Home page");
+        setPreferredSize(new Dimension(450, 450));
+        setBackground(Color.BLUE);
+        setVisible(true);
+        pack();
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+}
---
+++ src/LoginSystem/main.java
@@ -0,0 +1,12 @@
+package LoginSystem;
+
+public class main {
+
+    public static void main(String[] args) {
+        Login in = new Login();
+        //HomePage Page = new HomePage();
+
+    }
+
+
+}
